74367e67bb6767bf8c4439d59edcbed397976827
hachisanju <thomas@meltchannel.com>
2016-03-16 17:13:14 -0400

    Added a brief bit of code that will be expanded into a "Murder Scenario Generator". Most importantly, added a small persistant datapath serializer for storing save file info. All of the save file info from the generator should be stored in new games as sort of the "seed" for each save file.

---
+++ Waystation/Assets/Generator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("Generator")]
+public class Generator {
+
+	[XmlAttribute("murderer")]
+	public string Murderer;
+
+	public void Write(string path) {
+		var serializer = new XmlSerializer (typeof(Generator));
+		var stream = new FileStream(path, FileMode.Create);
+		serializer.Serialize(stream, this);
+		stream.Close();
+	}
+}
---
+++ Waystation/Assets/Scenario.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Scenario : MonoBehaviour {
+
+	public Generator scenarioGenerator = new Generator();
+
+	// Use this for initialization
+	void Start () {
+		
+		scenarioGenerator.Murderer = "test";
+		scenarioGenerator.Write (Path.Combine (Application.persistentDataPath, "save1.xml"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
