ca2ca203b2f7ec796a178113de6ca937a511ba6d
hachisanju <thomas@meltchannel.com>
2016-03-17 09:49:11 -0400

    Added an actual random number generator to the generator.

--- Waystation/Assets/Generator.cs
+++ Waystation/Assets/Generator.cs
@@ -7,10 +7,16 @@ using System.IO;
 [XmlRoot("Generator")]
 public class Generator {
 
+	public string[] Townspeople = new string[] {"Greg", "Dave",
+	"Sarah", "Freddy", "Alex", "Ryan", "Jen"};
+	
+
 	[XmlAttribute("murderer")]
 	public string Murderer;
 
 	public void Write(string path) {
+		int gen = Random.Range (0, 6);
+		Murderer = Townspeople [gen];
 		var serializer = new XmlSerializer (typeof(Generator));
 		var stream = new FileStream(path, FileMode.Create);
 		serializer.Serialize(stream, this);
